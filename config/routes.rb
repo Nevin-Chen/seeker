18cedaf61157cfadc6a12a9c98f2a034467880f3
nevin-chen <nevinchen6@gmail.com>
2025-12-17 19:00:34 -0500

    add login and logout alias to sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   root "home#index"
   get "home/index"
 
-  get "registrations/new"
-  get "registrations/create"
-  resource :session
-  resources :passwords, param: :token
-
   get  "signup", to: "registrations#new", as: :signup
   post "signup", to: "registrations#create"
+
+  resource :session, only: [ :create, :destroy ]
+  get "login", to: "sessions#new"
+  delete "logout", to: "sessions#destroy"
+
+  resources :passwords, param: :token
 end
